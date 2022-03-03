ebf007cc5a5cb62f30e3bed891f38e540c3f488d
nghiaagent <nghia6a1@gmail.com>
2022-03-03 10:16:29 +1000

    Added plotly

--- Koch-snowflake.R
+++ Koch-snowflake.R
@@ -3,23 +3,25 @@ if (!require("pacman"))
   install.packages("pacman")
 library(pacman)
 
-pacman::p_load(tidyverse, alphahull)
+pacman::p_load(tidyverse, alphahull, ragg, plotly)
 
 #### Draw Koch snowflake ####
 ### Generate list of vertices ###
 vertices <- koch(side = 2,
-                 niter = 3) %>%
+                 niter = 5) %>%
   as_tibble()
 
 vertices <- rbind(vertices,
                   head(vertices, 1))
 
 ### Draw convex hull (shape bound by vertices) ###
-ggplot(vertices,
-       aes(x = `V1`, y = `V2`)) +
-  geom_point() +
+plot <- ggplot(vertices,
+               aes(x = `V1`, y = `V2`)) +
+  geom_point(size = 0.5) +
   geom_path() +
   theme_bw() +
   theme(axis.title.x = element_blank(),
-        axis.title.y = element_blank(),) +
+        axis.title.y = element_blank(), ) +
   coord_fixed(ratio = 1)
+
+ggplotly(plot)
